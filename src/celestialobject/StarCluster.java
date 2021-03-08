89443e52416cc07011f2c46d890f86ce1ffbee49
Sleepyawk <78984417+Sleepyawk@users.noreply.github.com>
2021-03-07 16:05:43 -0800

    Update StarCluster.java

--- src/celestialobject/StarCluster.java
+++ src/celestialobject/StarCluster.java
@@ -1,5 +1,25 @@
 package src.celestialobject;
 
 public class StarCluster {
-    
+	private int starCount;
+	private String clusterType;
+	
+		public void setStarCount(int stars){
+			starCount = stars;
+		}
+		public void setClusterType(String cluster){
+			clusterType = cluster;
+		}
+		public void addStar(){
+			
+		}
+		public void removeStar(){
+			
+		}
+		public int getStarCount(){
+			return starCount;
+		}
+		public String getClusterType(){
+			return clusterType;
+		}
 }
